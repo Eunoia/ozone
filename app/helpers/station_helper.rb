22551ca47819f21c64d8cc25619e5e29c0fb3a6d
Evan R <eunoia.github@gmail.com>
2016-02-03 12:27:43 -0700

    Show map of area around station

--- app/helpers/station_helper.rb
+++ app/helpers/station_helper.rb
@@ -1,2 +1,7 @@
 module StationHelper
+	def static_map(marker)
+		root = 'https://maps.googleapis.com/maps/api/staticmap'
+		key = 'AIzaSyBtxj21zNBckjM1RCBV8hpb8dRXyEngkcE'
+		"#{root}?markers=#{marker}&zoom=14&size=320x320&key=#{key}"
+	end
 end
